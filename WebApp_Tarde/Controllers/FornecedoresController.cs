a61679f7e2650f9c1df8069dc0519db50c7578cf
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-04-01 18:52:01 -0300

    - Criação do controller (Fornecedores/Produtos)  - Criação das views (Lista Fornecedores/Produtos) e (Cadastro Fornecedores/Produtos) - Adição dos links para cada página no dropdown da nav bar em "Layout"

---
+++ WebApp_Tarde/Controllers/FornecedoresController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
