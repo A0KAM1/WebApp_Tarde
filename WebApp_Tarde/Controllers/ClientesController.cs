edb0ae4ec53eaed291fa9a4c589284a1c1131aab
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-04-19 15:58:11 -0300

    Botões de excluir, editar e cadastro de cliente

--- WebApp_Tarde/Controllers/ClientesController.cs
+++ WebApp_Tarde/Controllers/ClientesController.cs
@@ -1,16 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Tarde.Models;
 
 namespace WebApp_Tarde.Controllers
 {
     public class ClientesController : Controller
     {
+        public static List<ClientesViewModel> db = new List<ClientesViewModel>();
         public IActionResult Lista()
         {
-            return View();
+            return View(db);
         }
         public IActionResult Cadastro()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClientesViewModel dados)
+        {
+            if (dados.ID == 0)
+            {
+                Random rand = new Random();
+                int aleat = rand.Next(1, 9999);
+                //gerando um ID aleatorio
+                dados.ID = aleat;
+                //adicionando os dados no "banco"
+                db.Add(dados);
+            }
+            else
+            {
+                int indice = db.FindIndex(a => a.ID == dados.ID);
+                db[indice] = dados;
+            }
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ClientesViewModel cliente = db.Find(a => a.ID == id);
+            if(cliente != null)
+            {
+                db.Remove(cliente);
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            ClientesViewModel cliente = db.Find(a => a.ID == id);
+            if(cliente != null)
+            {
+                return View(cliente);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
+
+
+        }
+
     }
 }
---
+++ WebApp_Tarde/Models/ClientesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp_Tarde.Models
+{
+    public class ClientesViewModel
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
