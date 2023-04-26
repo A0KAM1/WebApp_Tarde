91f875e338800499493e8cdcc6b8b70e6b99cd6d
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-04-26 16:57:44 -0300

    aula do dia 26/04 - ligação com o banco Produto + botão de cadastrar produtos

--- WebApp_Tarde/Controllers/ProdutosController.cs
+++ WebApp_Tarde/Controllers/ProdutosController.cs
@@ -1,17 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Tarde.Models;
+using WebApp_Tarde.Entidades;
 
 namespace WebApp_Tarde.Controllers
 {
     public class ProdutosController : Controller
     {
+        private readonly Contexto db;
+
+        public ProdutosController(Contexto opt)
+        {
+            db = opt;
+        }
         public IActionResult Lista()
         {
-            return View();
+            return View(db.Produtos.ToList());
         }
 
         public IActionResult Cadastro()
         {
             return View();
         }
+
+        public IActionResult Editar(int id)
+        {
+            return View(Cadastro);
+        }
+
+        public IActionResult SalvarDados(ProdutoEntidade dados)
+        {
+            if (dados.Id == 0)
+            {
+                db.Add(dados);
+                db.SaveChanges();
+            }
+            else
+            {
+                db.Update(dados);
+            }
+            return RedirectToAction("Lista");
+        }
+
     }
 }
