f010785ba5a0da43d7b95adf57de627df532631f
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-05-17 17:08:22 -0300

    .

--- WebApp_Tarde/Controllers/VendasController.cs
+++ WebApp_Tarde/Controllers/VendasController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Tarde.Entidades;
+using WebApp_Tarde.Models;
 
 namespace WebApp_Tarde.Controllers
 {
     public class VendasController : Controller
     {
-        public IActionResult Index()
+        private Contexto db;
+        public VendasController(Contexto contexto)
+        {
+            db = contexto;
+        }
+        public IActionResult Lista()
         {
             return View();
+        }
+        public IActionResult Cadastro()
+        {
+            NovaVendaViewModel model = new NovaVendaViewModel();
+            model.Lista_Produtos = db.Produtos.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult SalvarDados(VendasEntidade dados)
+        {
+
+            return View("Lista");
         }
     }
 }
--- WebApp_Tarde/Models/NovaVendaViewModel.cs
+++ WebApp_Tarde/Models/NovaVendaViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApp_Tarde.Models
+﻿using WebApp_Tarde.Entidades;
+namespace WebApp_Tarde.Models
 {
     public class NovaVendaViewModel
     {
+        public NovaVendaViewModel()
+        {
+            Lista_Produtos = new List<ProdutoEntidade>();
+        }
+        public List<ProdutoEntidade> Lista_Produtos { get; set; }
     }
 }
