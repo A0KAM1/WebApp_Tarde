2df8a8519572517e4c2b0ac2c7b96b17e9649ecc
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-05-17 17:08:58 -0300

    .

--- WebApp_Tarde/Entidades/ItensVendaEntidade.cs
+++ WebApp_Tarde/Entidades/ItensVendaEntidade.cs
@@ -2,5 +2,11 @@
 {
     public class ItensVendaEntidade
     {
+        public int Id { get; set; }
+        public int VendaId { get; set; }
+        public int ProdutoId { get; set; }
+        public ProdutoEntidade Produto { get; set; }
+        public VendasEntidade Venda { get; set; }
+        public decimal Valor { get; set; }
     }
 }
